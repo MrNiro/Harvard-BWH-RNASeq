3cd6b73202478645a437b1ccb9972523e27f74f5
Niro Lin <linqinyun@outlook.com>
2022-07-15 13:25:23 -0400

    fix create_sample.py

--- utils/create_sample.py
+++ utils/create_sample.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     input_sample = open("/home/ubuntu/rnaseq/samplesheet.csv", "w")
     input_sample.write("sample,fastq_1,fastq_2,strandedness\n")
     for _, _, filenames in os.walk(data_folder):
-        filenames.sort()
+        filenames.sort(key=lambda x: int(x.split("_")[0]))
         for idx, f_name in enumerate(filenames):
             sample = "control_REP%d," % (idx + 1)
             fastq = data_folder + f_name + ",,"
